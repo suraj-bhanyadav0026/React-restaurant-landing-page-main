4059ffb25515ccb50ca05f4a6dd0952c004a32c0
suraj-bhan2626 <surajbhanyadav0026@gmail.com>
2025-06-26 13:47:12 +0530

    ready to use

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+import 'swiper/css'
+import 'swiper/css/autoplay'
+import 'swiper/css/pagination'
+
+import '@/styles/globals.scss'
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
---
+++ pages/index.js
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+import Header from '@/components/Header'
+import Main from '@/components/Main'
+import Footer from '@/components/Footer'
+import Loader from '@/components/Loader'
+import ScrollUp from '@/components/ScrollUp'
+import styles from '@/styles/pages/Home.module.scss'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>React responsive restaurant landing page</title>
+        <meta
+          name='description'
+          content='React responsive restaurant landing page'
+        />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <Header />
+      <Main />
+      <Footer />
+      <Loader />
+      <ScrollUp />
+    </div>
+  )
+}
